0bdf300ef74c4840f585ca898fcbf16c2bc4f966
Frank Förster <frank.foerster@ime.fraunhofer.de>
2017-10-05 13:24:15 +0200

    Add description of the program

--- fastq-shuffle.pl
+++ fastq-shuffle.pl
@@ -3,3 +3,47 @@
 use strict;
 use warnings;
 
+=head1 fastq-shuffle.pl
+
+A small program to shuffle huge fastq files using external memory
+according to Sanders (1998) "Random Permutations on Distributed,
+External and Hierarchical Memory".
+
+=head1 SYNOPSIS
+
+    fastq-shuffle.pl -1 reads.fq -2 mates.fq
+
+=head1 OPTIONS
+
+=over 4
+
+=item -1/--reads
+
+Input file for first read.
+
+=item -2/--mates
+
+Input file for second read.
+
+=item -t/--num-temp-files [0/auto]
+
+Number of temporary files, the input is split in. The split files are
+loaded into memory entirely for shuffling. A value of 0 or auto calulates the number of temporary files based on the shuffle block size
+
+=item -s/--shuffle-block-size [1G]
+
+The size of a single shuffle block. The entire input will be split
+into blocks of that size in bytes. Unit signs might be used for
+mega-(m/M), kilo-(k/K), or giga-(g/G) byte. The default value is 1
+gigabyte.
+
+=item -d/--temp-directory
+
+The temporary files are created inside the given folder. One might use
+that option to put the temporary files onto fast disks, eg. SSDs or
+into a RAM disk.
+
+=back
+
+=cut
+
